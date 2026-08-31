44f20c161e97470051718f6924318461a7884d37
Jameslurtin <jameslurtin@gmail.com>
2026-08-31 18:22:56 +0200

    Prepare backend for cloud deployment

--- backend/main.py
+++ backend/main.py
@@ -8,12 +8,13 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 create_table()
 
 
